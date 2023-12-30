f6f9e70b68470422e8e9ab19120316799fcd17f9
web.varlamov <web.varlamov@yandex.ru>
2023-12-30 16:27:07 +0400

    Init

--- src/main/java/web/varlamov/dockerimagedemo/controller/HelloController.java
+++ src/main/java/web/varlamov/dockerimagedemo/controller/HelloController.java
@@ -1,5 +1,13 @@
 package web.varlamov.dockerimagedemo.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("hello")
 public class HelloController {
   @GetMapping
-  public String get() {
+  public ResponseEntity<?> get() throws IOException {
     System.out.println("Handle new request: " + System.currentTimeMillis());
-    return "Hello from Docker!";
+    File videoFile = ResourceUtils.getFile("classpath:static/video.mp4");
+    byte[] imageBytes = Files.readAllBytes(videoFile.toPath());
+
+    return ResponseEntity.status(HttpStatus.OK)
+        .header(HttpHeaders.CONTENT_DISPOSITION, "filename=\"video.mp4\"")
+        .contentType(new MediaType("video", "mp4"))
+        .body(imageBytes);
   }
 }
