f1a7d0febfccb1bce17f827cd13378677e3489c4
web.varlamov <web.varlamov@yandex.ru>
2024-01-02 15:40:12 +0400

    Init

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,2 +1,2 @@
-docker build --no-cache -t cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.10 .
-docker push cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.10
+docker build --no-cache -t cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.13 .
+docker push cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.13
