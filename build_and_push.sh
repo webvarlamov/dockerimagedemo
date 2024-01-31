998fedb459b82bd23ca1c28596d34c639f5744df
web.varlamov <web.varlamov@yandex.ru>
2024-01-31 20:42:58 +0400

    Init

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,1 +1,2 @@
 docker build --no-cache -t cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.16 .
+docker push cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.16
--- push.sh
+++
@@ -1,1 +0,0 @@
-docker push cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.16
