4762c9bc2f8af715319a306a371b2b948799f1fb
Роман Варламов <web.varlamov@yandex.ru>
2025-03-01 15:49:44 +0400

    Доработки перед школой

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,2 +1,2 @@
-docker build --no-cache -t cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.16 .
-docker push cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.16
+docker build --no-cache -t cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.1 --platform linux/amd64 .
+docker push cr.yandex/crp3g2ebtfsh3buhv6q0/dockerimagedemo:0.0.1
